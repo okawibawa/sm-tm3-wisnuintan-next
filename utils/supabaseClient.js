a861ff8e0d82a9c5465da4947a2c1bc4fe5b839e
Oka Wibawa <okaa.wibawa@gmail.com>
2022-03-24 23:46:30 +0800

    fix error

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  'https://xkwpictlhsutjcrciiaq.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhrd3BpY3RsaHN1dGpjcmNpaWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDcwOTYyMjQsImV4cCI6MTk2MjY3MjIyNH0.dkYmYinUrIp39cEEG7YvBiWHKPQcCw5Fy7PQHF9sGKc'
+);
